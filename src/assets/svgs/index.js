fe969ab1ff7b39855e0c309a169f457eb54a68fb
parth chugh <parthchugh52@gmail.com>
2022-01-29 18:15:20 +0530

    removed unwanted icos

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,71 +1,8 @@
+import BackButton from './BackButton';
+import AuthType from './AuthType';
+import Address from './Address';
+import Right from './RightIcon';
 import Communication from './Communications';
 import GetStarted from './GetStarted';
-import Login from './MenPhone';
-import AuthType from './AuthType';
-import UploadImage from './UploadImage';
-import Calendar from './Calendar';
-import Male from './Male';
-import Female from './Female';
-import Filter from './Filter';
-import FlameFocus from './FlameFocus';
-import Flame from './Flame';
-import Chats from './Chats';
-import ChatsFocused from './ChatsFocused';
-import Profile from './Profile';
-import ProfileFocused from './ProfileFocused';
-import Match from './Match';
-import Dots from './Dots';
-import Search from './Search';
-import EmptyChats from './EmptyChats';
-import Arrow from './Arrow';
-import Info from './Info';
-import Share from './Share';
-import Facebook from './Facebook';
-import Instagram from './Instagram';
-import Twitter from './Twitter';
-import Camera from './Camera';
-import Audio from './Audio';
-import Map from './Map';
-import Send from './Send';
-import BackButton from './BackButton';
-import CameraUpload from './CameraUpload'
-import Check from './Check'
-import Address from './Address'
-import Right from './RightIcon'
 
-export {
-  Communication,
-  GetStarted,
-  Login,
-  AuthType,
-  UploadImage,
-  Calendar,
-  Male,
-  Female,
-  Filter,
-  Flame,
-  FlameFocus,
-  Chats,
-  ChatsFocused,
-  Profile,
-  ProfileFocused,
-  Match,
-  Dots,
-  Search,
-  EmptyChats,
-  Arrow,
-  Info,
-  Share,
-  Facebook,
-  Instagram,
-  Twitter,
-  Camera,
-  Audio,
-  Map,
-  Send,
-  BackButton,
-  CameraUpload,
-  Check,
-  Right,
-  Address
-};
+export {AuthType, BackButton, Address, Right, GetStarted, Communication};
